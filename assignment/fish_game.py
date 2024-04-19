14e60dd6d79bb305505c8141baea2e3c9f9ea0bc
THELEGENDARYETHANDO <ethan.hartley2706@gmail.com>
2024-04-19 15:03:37 +0800

    music

--- assignment/fish_game.py
+++ assignment/fish_game.py
@@ -1,6 +1,7 @@
 import pygame
+pygame.mixer.init()
 
 def fish():
-    pygame.mixer.init()
-    pygame.mixer.load()
+    vine_boom = pygame.mixer.Sound("assignment\sound/vine.mp3")
+    pygame.mixer.Sound.play(vine_boom)
     return(pygame.image.load("assignment/pictures/fish1.png"))
